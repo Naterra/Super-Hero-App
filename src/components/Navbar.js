060af00db06519bdcc1f2a30a9f027bdf3e797f4
Naterra <natalinyweb@hotmail.com>
2019-04-16 22:10:27 -0400

    initial Layout and Navbar components

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Layout extends Component {
+	render() {
+		return (
+				<nav>
+					<div className="nav-wrapper teal lighten-2">
+						<a href="#" className="brand-logo">
+							Super Hero
+						</a>
+					</div>
+				</nav>
+		);
+	}
+}
+export default Layout;
---
+++ src/pages/Homepage/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+
+import Layout from '../../components/Layout';
+import AuthComponent from '../../components/AuthComponent';
+
+class App extends Component {
+	render() {
+		return (
+			<Layout>
+				<h4>Welcome to Super Hero Command Center!</h4>
+
+				
+			</Layout>
+		);
+	}
+}
+
+export default App;
