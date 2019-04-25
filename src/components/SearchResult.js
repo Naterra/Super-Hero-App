f74ffb3d4c5ac20bd33146935ca4f5a3fb41c527
Naterra <natalinyweb@hotmail.com>
2019-04-24 23:23:53 -0400

    updated SearchResult component

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
 
 class SearchResult extends Component {
-	render() {
+	constructor() {
+		super();
+		this.onClickEvent = this.onClickEvent.bind(this);
+		this.recordsBlock = this.recordsBlock.bind(this);
+	}
+	onClickEvent(record) {
+		const { records, comparisonView, setHero, setVillain } = this.props;
+		if (comparisonView.selectedAlignment == 'hero') {
+			setHero(record);
+		} else {
+			setVillain(record);
+		}
+		console.warn('onClickEvent', record);
+	}
+	recordsBlock() {
 		const { records } = this.props;
 
-        return (<div className="row">
-				{records && records.map((item, i)=>{
-					return (<div key={i} className="card horizontal">
-						<div className="card-image">
-							<img className="responsive-img" src={item.image.url} style={{height: "90px"}} />
-						</div>
+		return (
+			<div>
+				{records &&
+					records.map((item, i) => {
+						return <SearchResultItem onClickEvent={this.onClickEvent} key={i} data={item} />;
+					})}
+			</div>
+		);
+	}
+	render() {
+		const { records, comparisonView } = this.props;
+		console.error('SearchResult', this.props);
+		const title = comparisonView.selectedAlignment === 'hero' ? "List of Heroes":(comparisonView.selectedAlignment === 'villain' ? "List of Villains":"");
 
-						<div></div>
-						<div>name:{item.name}</div>
-						<div>alignment:{item.biography.alignment}</div>
-
-					</div>);
-				})}
-            </div>
+		return (
+			<div>
+				{records.length > 0 && (<h5>{title}</h5>)}
+				<div style={{ overflow: 'scroll', height: '400px', padding: '0 2px' }}>{records.length > 0 && this.recordsBlock()}</div>
+			</div>
 		);
 	}
 }
+
+const SearchResultItem = props => {
+	const { data, onClickEvent } = props;
+
+	return (
+		<div className="card horizontal" onClick={e => onClickEvent(data)}>
+			<div className="card-image">
+				<img className="responsive-img" src={data.image.url} style={{ height: '90px' }} />
+			</div>
+
+			<div>{data.name}</div>
+		</div>
+	);
+};
 
 export default SearchResult;
