d050dafef73823844a547c47477258646aef04fa
Naterra <natalinyweb@hotmail.com>
2019-04-24 23:20:50 -0400

    updated Searchbox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,23 +2,79 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 /**  Actions  **/
-import { searchByName } from '../store/actions';
+import Preloader from './Preloader';
 
+/**  Actions  **/
+import { searchByName, resetSearchResult } from '../store/actions';
 
 class SearchBox extends Component {
-	onChangeEvent(val){
-		console.warn("search input changed", val);
-		this.props.searchByName(val);
+	constructor() {
+		super();
+
+		this.state = {
+			error: null,
+			loading: null,
+			typingTimeout: null
+		};
+
+		this.onChangeEvent = this.onChangeEvent.bind(this);
+		this.fetchData = this.fetchData.bind(this);
+	}
+	fetchData(keyword) {
+		//reset store records to empty list
+		this.props.resetSearchResult();
+
+		//change state
+		this.setState({
+			loading: true,
+			error: null
+		});
+
+		//search for records
+		this.props
+			.searchByName(keyword)
+			.then(res => {
+				if (res.data.err) this.setState({ error: res.data.err });
+			})
+			.catch(err => {})
+			.finally(() => {
+				this.setState({ loading: false });
+			});
+	}
+	onChangeEvent(keyword) {
+		console.warn('search input changed', keyword);
+		const { typingTimeout } = this.state;
+
+		if (typingTimeout) {
+			clearTimeout(typingTimeout);
+		}
+
+		this.setState({
+			// keyword: keyword,
+			typingTimeout: setTimeout(() => {
+				if (keyword) {
+					this.fetchData(keyword);
+				} else {
+					//reset to empty list
+					this.props.resetSearchResult();
+				}
+			}, 800)
+		});
 	}
 	render() {
-        return (<div className="row">
-			<div className="input-field col s12">
-				<input onChange={e=>this.onChangeEvent(e.target.value)} placeholder="Character Name" id="first_name" type="text" />
-				<span className="helper-text">Search by character Name</span>
+		const { error, loading } = this.state;
+
+		return (
+			<div className="row">
+				<div className="input-field col s12">
+					<input onChange={e => this.onChangeEvent(e.target.value)} placeholder="Character Name" id="first_name" type="text" autoComplete="off"/>
+					<span className="helper-text">Search by character Name</span>
+				</div>
+				{error && <p className="center">{error}</p>}
+				{loading && <Preloader/>}
 			</div>
-            </div>
 		);
 	}
 }
 
-export default connect(null, { searchByName })(SearchBox);
+export default connect(null, { searchByName, resetSearchResult })(SearchBox);
