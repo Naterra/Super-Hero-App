db952d80fe0ad4c1985b823f1a659b1110c85c4d
Naterra <natalinyweb@hotmail.com>
2019-04-24 20:58:12 -0400

    Tabs component

---
+++ src/components/Tabs.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Tabs extends React.Component {
+	constructor(props) {
+		super(props);
+		this.tabRef = React.createRef();
+		this.tabsInstance = null;
+	}
+
+	componentDidMount() {
+		const { id } = this.props;
+		const tabsEl = this.tabRef.current;
+		this.tabsInstance = window.M.Tabs.init(tabsEl, {});
+	}
+
+	render() {
+		const { id, menuList, children } = this.props;
+
+		return (
+			<div className="row">
+				<div className="col s12 tab-menu">
+					<ul id={id} ref={this.tabRef} className="tabs tabs-fixed-width">
+						{menuList.map((item, i) => {
+							return (
+								<li className="tab ">
+									<a className={i == 0 ? 'active' : ''} href={`#tab${id}-${i + 1}`}>
+										{item}
+									</a>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+				{children}
+			</div>
+		);
+	}
+}
+
+export default Tabs;
